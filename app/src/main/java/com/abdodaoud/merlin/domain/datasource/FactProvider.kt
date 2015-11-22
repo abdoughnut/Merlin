4a1f142c1fee30d48a2306756a00bef642a15881
Abdo Daoud <abdo@daoud.co>
2015-11-22 18:52:53 +0200

    Fixed timing to be tomorrow not today.

--- app/src/main/java/com/abdodaoud/merlin/domain/datasource/FactProvider.kt
+++ app/src/main/java/com/abdodaoud/merlin/domain/datasource/FactProvider.kt
@@ -1,5 +1,6 @@
 package com.abdodaoud.merlin.domain.datasource
 
+import android.text.format.DateUtils
 import com.abdodaoud.merlin.data.db.FactDb
 import com.abdodaoud.merlin.data.server.FactServer
 import com.abdodaoud.merlin.domain.model.Fact
@@ -14,13 +15,15 @@ class FactProvider(val sources: List<FactDataSource> = FactProvider.SOURCES) {
     }
 
     fun request(days: Int): FactList = requestToSources {
-        val res = it.request(todayTimeSpan())
+        val res = it.request(tomorrowTimeSpan())
         if (res != null && res.size() >= days) res else null
     }
 
     fun requestFact(id: Long): Fact = requestToSources { it.requestDayFact(id) }
 
-    private fun todayTimeSpan() = System.currentTimeMillis() / DAY_IN_MILLIS * DAY_IN_MILLIS
+    private fun tomorrowTimeSpan() = System.currentTimeMillis() + DateUtils.DAY_IN_MILLIS +
+            (DateUtils.DAY_IN_MILLIS - System.currentTimeMillis() % DateUtils.DAY_IN_MILLIS) /
+                    DAY_IN_MILLIS * DAY_IN_MILLIS
 
     private fun <T : Any> requestToSources(f: (FactDataSource) -> T?): T = sources.firstResult { f(it) }
 
